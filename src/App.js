57d8d3ab18e53bbb753e87b0a5e9762e2ad588b1
jeoman <kremav1@naver.com>
2018-04-05 23:09:52 +0900

    Movie product

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Album from './Album';
+import Movie from './Movie';
 
 
 
@@ -9,49 +9,44 @@ class App extends Component {
   //render : componentWillMount() -> render() -> componentDidMount()
   //update : componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() ->componentDidUpdate() 
   
-  state ={  
-  }
+  state ={  }
 
   componentDidMount(){
-    setTimeout(()=>{
-      this.setState({
-        albums : [
-          {
-            title : "Dailylook",
-            poster :  "https://i0.wp.com/klyrics.net/wp-content/uploads/2017/02/Hash-Swan-Shangri-La.jpg?fit=600%2C600&ssl=1"
-          },
-          {
-            title : "We Here 2",
-            poster :  "https://i.ytimg.com/vi/ZLvgSetReqk/hqdefault.jpg"
-          },
-          {
-            title : "Francesca",
-            poster : "https://i2.wp.com/klyrics.net/wp-content/uploads/2018/02/Hash-Swan-Alexandrite.jpg?fit=600%2C600&ssl=1"
-          },
-          {
-            title :  "One more rollies",
-            poster :  "https://2.bp.blogspot.com/-pT9nzqMwALk/WSU36xgHJNI/AAAAAAAAIzo/3fbkG7CIigYuSaACZg-2aC4uHN_1FzUHgCK4B/s1600/Lirik%2BLagu%2BCHANGMO%2B%25E2%2580%2593%2BOne%2BMore%2BRollie.jpg"
-          },
-          {
-            title : "dressroom",
-            poster : "https://images.genius.com/15d2414830253d3d42bcbc55a673d3c8.1000x1000x1.jpg"
-          }
-        ],
-      })
-    }, 2000)
+    this._getMovies();
   }
 
-  _renderAlbums = () =>{
-    const albums = this.state.albums.map((album,index)=>{
-      return <Album title={album.title} poster={album.poster} key={index} />
+  _renderMovies = () =>{
+    const movies = this.state.movies.map(movie => {
+      return <Movie 
+      title={movie.title_english} 
+      poster={movie.medium_cover_image} 
+      key={movie.id} 
+      genres={movie.genres}
+      synopsis={movie.synopsis}
+      />
     })
-    return albums
+    return movies
+  }
+
+  _getMovies = async() =>{
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () =>{
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+    .then(response =>response.json())
+    .then(json=> json.data.movies)
+    .catch(err=>console.log(err))
   }
 
   render() {
+    const {movies} = this.state;
     return (
-      <div className="App">
-        {this.state.albums ? this._renderAlbums() : 'Loading'}
+      <div className={movies ? "App" : "App--loading"}>
+        {movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
